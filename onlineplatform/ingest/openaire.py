1533b0e7a8969029d12ac26027c1bc89253756ba
Abdulqadir Rashik <abdulqadir.rashik@gmail.com>
2018-03-17 16:46:57 +0530

    Fetch results from OpenAire one day at a time

--- onlineplatform/ingest/openaire.py
+++ onlineplatform/ingest/openaire.py
@@ -6,8 +6,10 @@ if 'DJANGO_SETTINGS_MODULE' not in os.environ:
     os.environ['DJANGO_SETTINGS_MODULE'] = 'onlineplatform.settings'
 import django
 django.setup()
-from sti.models import Publication
+from sti.models import Store
 from ingest.models import Metadata
+from datetime import date, timedelta
+from django.contrib.postgres.search import SearchVector
 
 def scrape():
     try:
@@ -17,34 +19,53 @@ def scrape():
         metadata.partner = 'openaire'
         metadata.keyval = {'nextDate':'2015-01-01'}
         metadata.save()
-    fromDate = metadata.keyval['nextDate'].split('-')
-    for page in range(1,1001):
-        r = requests.get('http://api.openaire.eu/search/publications?fromDateAccepted=2015-01-01&toDateAccepted=2017-12-31&page='+str(page))
-        if r.status_code == 200:
-            root = ElementTree.fromstring(r.text)
-            for result in root.findall('results/result'):
-                publication = Publication()
-                publication.client_id = result.find('header/{http://www.driver-repository.eu/namespace/dri}objIdentifier').text
-                publication.title = result.find('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/title').text
-                publication.description = '\n'.join(map(lambda d:d.text, filter(lambda d:d.text != None, result.findall('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/description'))))
-                print(publication.client_id, publication.title, publication.description)
-                publication.raw_data = ElementTree.tostring(result)
-                publication.save()
+    while True:
+        frm = list(map(lambda s:int(s), metadata.keyval['nextDate'].split('-')))
+        nxt = date(frm[0], frm[1], frm[2]) + timedelta(days=1)
+        if nxt > date(2018,1,1):
+            return;
+        toDate = nxt.strftime('%Y-%m-%d')
+        for page in range(1,1001):
+            url = 'http://api.openaire.eu/search/publications?fromDateAccepted='+ metadata.keyval['nextDate'] + '&toDateAccepted='+ toDate +'&page='+str(page)
+            r = requests.get(url)
+            if r.status_code == 200:
+                print(url)
+                root = ElementTree.fromstring(r.text)
+                results = root.findall('results/result')
+                if len(results) == 0:
+                    break
+                for result in results:
+                    client_id = result.find('header/{http://www.driver-repository.eu/namespace/dri}objIdentifier').text
+                    try:
+                        publication = Store.objects.get(client_id = client_id)
+                    except Store.DoesNotExist:
+                        publication = Store()
+                        publication.client_id = client_id
+                    publication.partner = 'OpenAire'
+                    publication.store_type = 'Publication'
+                    print(publication.client_id)
+                    publication.raw_data = ElementTree.tostring(result)
+                    publication.save()
+        metadata.keyval = {'nextDate': toDate}
+        metadata.save()
 
-def language():
-    update = Publication.objects.all().filter(language='')
+def parse():
+    update = Store.objects.all().filter(title='')
     for publication in update:
         result = ElementTree.fromstring(publication.raw_data)
+        publication.title = result.find('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/title').text
+        if len(publication.title) > 2048:
+            publication.title = publication.title[:2048]
+        publication.description = '\n'.join(map(lambda d:d.text, filter(lambda d:d.text != None, result.findall('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/description'))))
         language = result.find('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/language')
-        if language != None:
-            language = language.get('classname').lower().split(';')[0]
-            if language in ['danish', 'dutch', 'english', 'finnish', 'french', 'german', 'hungarian', 'italian', 'norwegian', 'portuguese', 'romanian', 'russian', 'spanish', 'swedish', 'turkish']:
-                publication.language = language
-                print(publication.language)
-                publication.save()
-                continue
-        publication.language = ''
+        config = 'simple'
+        language = language.get('classname').lower().split(';')[0]
+        if language in ['danish', 'dutch', 'english', 'finnish', 'french', 'german', 'hungarian', 'italian', 'norwegian', 'portuguese', 'romanian', 'russian', 'spanish', 'swedish', 'turkish']:
+            config = language
+        publication.language = language
+        print(publication.language, config, publication.title)
+        publication.save()
+        publication.search_vector = SearchVector('title', weight='A', config=config) + SearchVector('keywords', weight='D', config=config) + SearchVector('description', weight='C', config=config)
         publication.save()
 
-
-language()
+scrape()
