240b96ea69c21525ee988b3bed257308dccfff3e
Abdulqadir Rashik <abdulqadir.rashik@gmail.com>
2018-03-18 11:27:34 +0530

    Parse thousand records at a time to prevent running out of memory

--- onlineplatform/ingest/openaire.py
+++ onlineplatform/ingest/openaire.py
@@ -50,22 +50,25 @@ def scrape():
         metadata.save()
 
 def parse():
-    update = Store.objects.all().filter(title='')
-    for publication in update:
-        result = ElementTree.fromstring(publication.raw_data)
-        publication.title = result.find('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/title').text
-        if len(publication.title) > 2048:
-            publication.title = publication.title[:2048]
-        publication.description = '\n'.join(map(lambda d:d.text, filter(lambda d:d.text != None, result.findall('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/description'))))
-        language = result.find('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/language')
-        config = 'simple'
-        language = language.get('classname').lower().split(';')[0]
-        if language in ['danish', 'dutch', 'english', 'finnish', 'french', 'german', 'hungarian', 'italian', 'norwegian', 'portuguese', 'romanian', 'russian', 'spanish', 'swedish', 'turkish']:
-            config = language
-        publication.language = language
-        print(publication.language, config, publication.title)
-        publication.save()
-        publication.search_vector = SearchVector('title', weight='A', config=config) + SearchVector('keywords', weight='D', config=config) + SearchVector('description', weight='C', config=config)
-        publication.save()
+    while True:
+        update = Store.objects.all().filter(title='')[:1000]
+        if len(update) == 0:
+            return
+        for publication in update:
+            result = ElementTree.fromstring(publication.raw_data)
+            publication.title = result.find('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/title').text
+            if len(publication.title) > 2048:
+                publication.title = publication.title[:2048]
+            publication.description = '\n'.join(map(lambda d:d.text, filter(lambda d:d.text != None, result.findall('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/description'))))
+            language = result.find('metadata/{http://namespace.openaire.eu/oaf}entity/{http://namespace.openaire.eu/oaf}result/language')
+            config = 'simple'
+            language = language.get('classname').lower().split(';')[0]
+            if language in ['danish', 'dutch', 'english', 'finnish', 'french', 'german', 'hungarian', 'italian', 'norwegian', 'portuguese', 'romanian', 'russian', 'spanish', 'swedish', 'turkish']:
+                config = language
+            publication.language = language
+            print(publication.language, config, publication.title)
+            publication.save()
+            publication.search_vector = SearchVector('title', weight='A', config=config) + SearchVector('keywords', weight='D', config=config) + SearchVector('description', weight='C', config=config)
+            publication.save()
 
 scrape()
