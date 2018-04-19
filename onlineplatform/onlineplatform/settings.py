71d73c04370f4f114f2a81a09766e01fd9c1fb82
Abdulqadir Rashik <abdulqadir.rashik@gmail.com>
2018-04-19 17:19:53 +0530

    Increase cache timeout to 10 hours

--- onlineplatform/onlineplatform/settings.py
+++ onlineplatform/onlineplatform/settings.py
@@ -129,6 +129,6 @@ CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
             'LOCATION': '/var/tmp',
-            'TIMEOUT': 3600,
+            'TIMEOUT': 36000,
         }
 }
