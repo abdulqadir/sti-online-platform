316ff2cf2e0a26d52bb7181f4a24fa4ace539fcc
Abdulqadir Rashik <abdulqadir.rashik@gmail.com>
2018-05-02 18:26:12 +0530

    Remove models for ingest app and make fresh migrations

--- onlineplatform/ingest/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.postgres.fields import JSONField
-
-class Metadata(models.Model):
-    partner = models.CharField(max_length=128, unique=True)
-    keyval = JSONField()
